a9e29917ec0bc3d6a3c523f90f4dc63c755598e0
nickwalker037 <nickwalker037@gmail.com>
2016-12-18 14:59:08 -0500

    Create Underfitting vs. Overfitting.py

---
+++ Model Selection/Underfitting vs. Overfitting.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+np.random.seed(0)
+
+n_samples = 30
+degrees = [1, 4, 15]
+
+data = lambda X: np.cos(1.5 * np.pi * X)
+X = np.sort(np.random.rand(n_samples))
+Y = data(X) + np.random.randn(n_samples) * 0.1
+
+plt.figure(figsize=(14, 5))
+for i in range(len(degrees)):
+    ax = plt.subplot(1, len(degrees), i + 1)
+    plt.setp(ax, xticks=(), yticks=())
+
+    polynomial_features = PolynomialFeatures(degree=degrees[i], include_bias=False)
+    linear_regression = LinearRegression()
+    pipeline = Pipeline([("polynomial_features", polynomial_features),
+                         ("linear_regression", linear_regression)])
+    pipeline.fit(X[:, np.newaxis], Y)
+
+    # Evaluate the models using cross-validation:
+    scores = cross_val_score(pipeline, X[:, np.newaxis], Y, scoring="neg_mean_squared_error", cv=10)
+
+    X_test = np.linspace(0, 1, 100)
+    plt.plot(X_test, pipeline.predict(X_test[:, np.newaxis]), label="Model")
+    plt.plot(X_test, data(X_test), label="True Function")
+    plt.scatter(X, Y, label="Samples")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.xlim((0, 1))
+    plt.ylim((-2, 2))
+    plt.legend(loc="best")
+    plt.title("Degree {}\nMSE = {:.2e}(+/- {:.2e})".format(degrees[i], -scores.mean(), scores.std()))
+
+plt.show()
